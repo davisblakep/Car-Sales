85d572049e1ca48e0cf5543c13b78deebaf15f62
Blake Davis <blakedavis@Blakes-MBP.lan1>
2020-06-21 21:53:25 -0400

    Added additional connect elements to bring state into components.  Not all components are imported, but most are

--- src/components/AdditionalFeatures.js
+++ src/components/AdditionalFeatures.js
@@ -1,21 +1,30 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
 import AdditionalFeature from './AdditionalFeature';
+
 
 const AdditionalFeatures = props => {
   return (
     <div className="content">
-      {/* <h4>Additional Features</h4>
-      {props.additionalFeatures.length ? (
+      <h4>Additional Features</h4>
+      {props.additionalFeaturesOnProps.length ? (
         <ol type="1">
-          {props.additionalFeatures.map(item => (
+          {props.additionalFeaturesOnProps.map(item => (
             <AdditionalFeature key={item.id} feature={item} />
           ))}
         </ol>
       ) : (
         <p>Nice looking car!</p>
-      )} */}
+      )}
     </div>
   );
 };
 
-export default AdditionalFeatures;
+const mapStateToProps = state => {
+  return{
+    additionalFeaturesOnProps: state.additionalFeatures,
+  }
+}
+
+export default connect(mapStateToProps)(AdditionalFeatures);
